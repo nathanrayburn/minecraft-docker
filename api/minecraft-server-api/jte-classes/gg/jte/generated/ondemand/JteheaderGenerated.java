6f9d8d2dfe2b588c79d7c001de98ebb0f241cae3
nathan <nathanarmand@hotmail.com>
2024-04-29 10:46:44 +0200

    update view

--- api/minecraft-server-api/jte-classes/gg/jte/generated/ondemand/JteheaderGenerated.java
+++ api/minecraft-server-api/jte-classes/gg/jte/generated/ondemand/JteheaderGenerated.java
@@ -3,7 +3,7 @@ public final class JteheaderGenerated {
 	public static final String JTE_NAME = "header.jte";
 	public static final int[] JTE_LINE_INFO = {3,3,3,3,3,3};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
-		jteOutput.writeContent("<header class=\"bg-white text-center py-16 mb-12\">\n    <h1 class=\"font-bold text-5xl mb-4\">API Minecraft Server Manager</h1>\n    <p class=\"text-gray-700\">Java implemented API for server management.</p>\n</header>");
+		jteOutput.writeContent("<header class=\"bg-white text-center py-16 mb-12\">\n    <h1 class=\"font-bold text-5xl mb-4\">API Server Manager</h1>\n    <p class=\"text-gray-700\">Java implemented API for server management.</p>\n</header>");
 	}
 	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
 		render(jteOutput, jteHtmlInterceptor);
---
+++ api/minecraft-server-api/src/main/static/navbar.js
@@ -0,0 +1,3 @@
+document.querySelector('.hamburger').addEventListener('click', function() {
+    document.getElementById('navbar').classList.toggle('open');
+});
